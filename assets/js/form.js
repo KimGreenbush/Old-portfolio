e9900aadcbcbc213c77f7a3c42e3c647155407dc
Kimberley <kimgreenbush@gmail.com>
2021-12-27 19:32:15 -0800

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -16,29 +16,40 @@ window.addEventListener("DOMContentLoaded", function () {
         status.innerHTML = "It looks like something went wrong."
     }
 
-    // form submission
-    // form.addEventListener("submit", function (e) {
-    //    e.preventDefault()
-    //    var data = new FormData(form)
-    //    ajax(form.method, form.action, data, success, error)
-    // })
+     form submission
+     form.addEventListener("submit", function (e) {
+        e.preventDefault()
+        var data = new FormData(form)
+        // ajax(form.method, form.action, data, success, error)
+        Email.send({
+            Host : "smtp.gmail.com",
+            Username : env.USERNAME,
+            Password : env.PASSWORD ,
+            To : 'kimberley@tsukinomegami.com',
+            From : form.email,
+            Subject : form.name,
+            Body : form.message
+        }).then(
+          message => alert(message)
+        )
+     })
 })
 
 // ajax request
-function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest()
-    xhr.open(method, url)
-    xhr.setRequestHeader("Accept", "application/json")
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState !== XMLHttpRequest.DONE) return
-        if (xhr.status === 200) {
-            console.log("success")
-            success(xhr.response, xhr.responseType)
-        }
-        else {
-            console.log("error", xhr.status, xhr.response, xhr.responseType)
-            error()
-        }
-    }
-    xhr.send(data)
-}
+// function ajax(method, url, data, success, error) {
+//    var xhr = new XMLHttpRequest()
+//    xhr.open(method, url)
+//    xhr.setRequestHeader("Accept", "application/json")
+//    xhr.onreadystatechange = function () {
+//        if (xhr.readyState !== XMLHttpRequest.DONE) return
+//        if (xhr.status === 200) {
+//            console.log("success")
+//            success(xhr.response, xhr.responseType)
+//        }
+//        else {
+//            console.log("error", xhr.status, xhr.response, xhr.responseType)
+//            error()
+//        }
+//    }
+//    xhr.send(data)
+// }
